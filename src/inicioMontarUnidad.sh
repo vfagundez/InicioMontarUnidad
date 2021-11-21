94ad41c8510a08ac11a686e2f4e25e2d5fb0c722
Vincitori <vincitori.dev@gmail.com>
2021-11-21 11:06:08 +0100

    añadidas comprobaciones

--- src/inicioMontarUnidad.sh
+++ src/inicioMontarUnidad.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #-------------------------------------------------------------------------------
 # SCRIPT DE MONTAR UNIDAD
 #-------------------------------------------------------------------------------
@@ -7,40 +8,55 @@
 # Descripción: Script que monta la unidad de disco en el sistema
 # Run info:    Este script se ejecuta cada vez que se inicia el sistema
 #-------------------------------------------------------------------------------
-#!/usr/bin/env bash
-#-------------------------------------------------------------------------------
 # Variables locales
 #-------------------------------------------------------------------------------
     DIR="${BASH_SOURCE%/*}" #Directorio del script
+    FECHA=$(date +"%x-%X")  #Fecha y hora actual
 #-------------------------------------------------------------------------------
 # Funciones
 #-------------------------------------------------------------------------------
+function addMsg(){
+    # Agrega un mensaje a la lista de mensajes
+    echo "$FECHA: $1"
+}
+function checkUser(){
+    # Verifica si el usuario es root
+    if [ "$(id -u)" != "0" ]; then
+        addMsg "Este script debe ser ejecutado como root"
+        exit 1
+    fi
+}
 function addConfig(){
     # Comprobamos si existe el fichero de configuración
+    addMsg "Comprobando si existe el fichero de configuración"
     if [[ -e "$DIR/../etc/config.cfg" ]]; then
         # Si existe, conectamos el script con el fichero de configuración
             . "$DIR/../etc/config.cfg"
+            addMsg "Se ha conectado con el fichero de configuración"
     else
         # Si no existe, mostramos un mensaje de error
-            echo "El fichero de configuración no existe"
+            addMsg "No se ha encontrado el fichero de configuración"
+            addMsg "Se aborta la ejecución del script"
+            exit 1
     fi
 }
 function checkFolder(){
     # Comprobamos si existe el directorio de montaje
     if [[ -d "$DIR_TO_MOUNT_DISK" ]]; then
-            echo "El directorio $DIR_TO_MOUNT_DISK de montaje ya existe"
+            addMsg "El directorio $DIR_TO_MOUNT_DISK de montaje ya existe"
     else
         # Si no existe, lo creamos
-            echo "Creando el directorio $DIR_TO_MOUNT_DISK seleccionado"
-            sudo mkdir "$DIR_TO_MOUNT_DISK"
+            addMsg "Creando el directorio $DIR_TO_MOUNT_DISK seleccionado"
+            mkdir "$DIR_TO_MOUNT_DISK"
     fi
 }
 #-------------------------------------------------------------------------------
 # Main Script
 #-------------------------------------------------------------------------------
 function inicioMontarUnidad() {
+    checkUser # Verificamos si el usuario es root
     addConfig # Añadimos las variables de configuración
-    echo "Montando unidad de disco"
+    addMsg "Iniciando el script de montar unidad"
     checkFolder # Comprobamos si existe el directorio de montaje
     #montamos el disco duro en la carpeta que hemos creado
     mount -t ntfs "$DISK_TO_MOUNT" "$DIR_TO_MOUNT_DISK"
